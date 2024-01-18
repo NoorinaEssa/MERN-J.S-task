8017b740ea941269aac0e614c9aa5ae73a259394
Noorina <noorinaessa@gmail.com>
2024-01-18 13:33:32 +0500

    "datatypes"

--- 01_basics/01_dataTypes.js
+++ 01_basics/01_dataTypes.js
@@ -2,3 +2,23 @@
 
 consloe.log(3
     +3) // 
+     
+    console.log("Noor")
+
+    let name = ("Noor")
+    let age = 23
+    let state ;
+    /* primitives datatype*/
+
+    // number => to power 53
+    // bigint
+    //string =>""
+    //boolean => true/false
+    // nul =>standalone value
+    //undefined => no any value asign
+    //symbol => unique
+
+
+    // object
+
+    console.log(typeof null);
