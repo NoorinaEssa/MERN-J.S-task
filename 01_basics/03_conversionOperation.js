a0ea4b739ca1caf88d5bcd44bd2fe9ea6fb3f647
Noorina <noorinaessa@gmail.com>
2024-01-18 15:23:02 +0500

    dataconversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = true
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof(valueInNumber));
+
+console.log(valueInNumber);
+
+//"33"=>33
+//"33abc"=>NaN
+//TRUE => 1; FALSE=>0
+
+let isLoggedIn = "Noor"
+let booleanIsLoggedIn =  Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+// 1=>true; 0=>fasle
+// ""=>false
+//"noor"=> true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
