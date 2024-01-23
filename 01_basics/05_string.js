8a9bd14032f74e0ea4d6ccd03e64ec00a4106a27
Noorina <noorinaessa@gmail.com>
2024-01-23 11:27:11 +0500

    maths and num

---
+++ 01_basics/05_string.js
@@ -0,0 +1,29 @@
+const name = "Noor"
+const repoCount = 55
+
+console.log(name + repoCount + "value");
+//backtick const is modern way
+
+
+const gameName = new String ("Nooor")
+console.log(gameName + [0]);
+console.log(gameName .____proto___);
+console.log(gameName .toUpperCase());
+ console.log(gameName .charAt(2));
+
+ const newString = gameName.substring (0, 4)
+
+ console.log(newString);
+
+
+ const anotherString = gameName.slice (-8, 4)
+ console.log(anotherString);
+
+ const newStringOne = " norina   "
+//  console.log(newStringOne);
+// console.log(newStringOne,trim());
+const url = "http://mmajs.commmm"
+console.log(url.replace("mma", '_')); 
+
+
+
---
+++ 01_basics/06_num-and-maths.js
@@ -0,0 +1,47 @@
+const score = 400
+console.log(score);
+
+
+const balance = new Number(100);
+console.log(balance);
+
+
+console.log(balance.toString().length);
+
+console.log(balance.toFixed(1));
+
+
+const otherNumber = 2356.3333
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 10000
+
+//console.log(hundreds,localString('en-IN'));
+
+//...........mATHS.......................
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+
+console.log(Math.random());
+console.log((Math.random()*10 )+ 1);
+
+const min =  10
+const max = 20
+
+
+
+
+
+
+
+
+
+
+
+
