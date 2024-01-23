b5f7eb01a040268b5d87b3d8e4629fe596dc3677
Noorina <noorinaessa@gmail.com>
2024-01-23 12:54:53 +0500

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,34 @@
+//arrays
+ const myArr =[0, 1, 2, 3, 4, 5, 6, 7, true, "Noorina"];
+
+ const myHeros = ["Noorina", "Noor"]
+
+ const myArr2 = new Array(1,2,3,4,)
+ console.log(myArr[1]);
+
+ //arrays meothods
+
+//  myArr.push(6)
+//  myArr.push(7)
+//  myArr.pop(6)
+
+//  myArr.unshift(9)
+//  myArr.shift()
+
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(4));
+console.log(myArr.join());
+ console.log(myArr);
+
+ //slice, splice
+ console.log("A", myArr);
+ const  myn1 = myArr.slice(1, 3);
+ console.log(myn1);
+ console.log("B", myArr);
+
+ const  myn2 = myArr.splice(1, 3);
+ console.log(myn2);
+
+
+
+
